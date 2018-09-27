259a286c775d534acc5ebd0ac4cce4ae8e94b9cd
Patrick Venture <venture@google.com>
2018-09-27 15:00:46 -0700

    utils: add methods for finding and loading handlers
    This module provides a couple basic methods for enumerating and then
    loading handlers.
    
    Change-Id: I4f58be313190c48de25c2b9578b7c622afefc656
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ utils.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace blobs
+{
+
+/**
+ * @brief Given a path, find libraries (*.so only) and load them.
+ *
+ * @param[in] paths - list of fully qualified paths to libraries to load.
+ */
+void loadLibraries(const std::string& path);
+
+} // namespace blobs
