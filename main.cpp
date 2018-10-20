782e1b8dcdde81ac0a718e2908a14942f4d013d6
Benjamin Fair <benjaminfair@google.com>
2018-10-19 17:22:53 -0700

    main: catch exceptions when loading blob handlers
    If an error occurs while attempting to load the blob handlers (such as a
    filesystem error), the uncaught exception will crash the entire IPMI
    daemon. Instead, catch the exception and print an error message.
    
    Tested: ipmid doesn't crash when /usr/lib/blobs-ipmid is missing
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: Ic316f6e5aa3c4bc6eba4fa2e561b338f4b20f606

--- main.cpp
+++ main.cpp
@@ -75,6 +75,13 @@ void setupBlobGlobalHandler()
                                handleBlobCommand);
 
     /* Install handlers. */
-    loadLibraries(expectedHandlerPath);
+    try
+    {
+        loadLibraries(expectedHandlerPath);
+    }
+    catch (const std::exception& e)
+    {
+        std::fprintf(stderr, "Failed to load blob handlers: %s\n", e.what());
+    }
 }
 } // namespace blobs
