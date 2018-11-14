1e44353139b088b08fb7dca874225d7572a6daea
Patrick Venture <venture@google.com>
2018-11-14 14:01:36 -0800

    bugfix: load handlers and use factory symbol
    Use a predefined factory symbol to build each handler after loading the
    library.
    
    Todo: Run a test with a more narrow loading, using LAZY and LOCAL
    instead of GLOBAL for next patchset.
    Tested: Verified it was now able to load handlers and successfully call
    the factory and register the handlers.  This was run with two handlers,
    to verify they were fine loaded sequentially in this way.
    Change-Id: I0369c6e46a57c2e8533409d8b06eb74a3962434c
    Signed-off-by: Patrick Venture <venture@google.com>

--- utils.cpp
+++ utils.cpp
@@ -2,7 +2,9 @@
 
 #include <dlfcn.h>
 
+#include <blobs-ipmid/manager.hpp>
 #include <experimental/filesystem>
+#include <memory>
 #include <phosphor-logging/log.hpp>
 #include <regex>
 
@@ -15,6 +17,7 @@ using namespace phosphor::logging;
 void loadLibraries(const std::string& path)
 {
     void* libHandle = NULL;
+    auto* manager = getBlobManager();
 
     for (const auto& p : fs::recursive_directory_iterator(path))
     {
@@ -30,8 +33,36 @@ void loadLibraries(const std::string& path)
             continue;
         }
 
-        libHandle = dlopen(ps.c_str(), RTLD_NOW);
-        if (!libHandle)
+        libHandle = dlopen(ps.c_str(), RTLD_NOW | RTLD_GLOBAL);
+        if (libHandle)
+        {
+            dlerror(); /* Clear any previous error. */
+
+            std::unique_ptr<GenericBlobInterface> (*factory)(void);
+            factory = (std::unique_ptr<GenericBlobInterface>(*)(void))dlsym(
+                libHandle, "createHandler");
+
+            const char* error = dlerror();
+            if (error)
+            {
+                log<level::ERR>("ERROR loading symbol",
+                                entry("HANDLER=%s", ps.c_str()),
+                                entry("ERROR=%s", error));
+            }
+            else
+            {
+                std::unique_ptr<GenericBlobInterface> result = factory();
+                if (!result)
+                {
+                    log<level::ERR>("Unable to create handler",
+                                    entry("HANDLER=%s", ps.c_str()));
+                    continue;
+                }
+
+                manager->registerHandler(std::move(result));
+            }
+        }
+        else
         {
             log<level::ERR>("ERROR opening", entry("HANDLER=%s", ps.c_str()),
                             entry("ERROR=%s", dlerror()));
